0e74e0215cd9ab7766c8e09342441ff3b162f9df
david12-git <juandavidd33@gmail.com>
2026-01-06 21:30:16 -0500

    Update config.js

--- config.js
+++ config.js
@@ -104,7 +104,7 @@ const CONFIG = {
             id: 6,
             nombre: 'Cifrut',
             categoria: 'bebidas frias',
-            precio: 1110,
+            precio: 1100,
             precio_anterior: 1200,
             descripcion: 'Bebida refrescante de frutas tropicales.',
             imagen: 'imagen/Cifrut.jpg',
